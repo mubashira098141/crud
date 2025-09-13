64a81a7a3b2d587685bd50f80bc26d9d4f45dd65
muba <mubashiramirjamadar15@gmail.com>
2025-09-13 22:29:25 +0530

    commit

--- src/main/java/com/crud/controller/BookingController.java
+++ src/main/java/com/crud/controller/BookingController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 @RequestMapping("/bookings")
 public class BookingController {
 
-
+//@Autowired
     private BookingRepository bookingRepository;
 
     // ✅ Create a booking
